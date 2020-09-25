845534c8d4fe2bce7f833ee20b09697f651cfe21
Michael Sumner <mdsumner@gmail.com>
2020-09-25 07:20:20 +0000

    try rework

--- inst/include/gdalwarpmem/gdalwarpmem.h
+++ inst/include/gdalwarpmem/gdalwarpmem.h
@@ -1,19 +1,32 @@
 #ifndef GDALWARPMEM_H
 #define GDALWARPMEM_H
 #include <Rcpp.h>
+#include "gdal.h"
+#include "gdal_alg.h"
+#include "gdal_alg_priv.h"
 #include "gdal_priv.h"
 #include "gdalwarper.h"
+#include "gdal_utils.h"
+#include "gdal_version.h"
+//#include "commonutils.h"
+//#include "gdal_utils_priv.h"
+
 #include "cpl_conv.h" // for CPLMalloc()
 
 namespace gdalwarpmem{
 using namespace Rcpp;
+
+std::vector<char *> create_options(Rcpp::CharacterVector lco, bool quiet = true);
+
 inline List gdal_warp_in_memory(CharacterVector source_filename,
                      CharacterVector source_WKT,
                      CharacterVector target_WKT,
                      NumericVector target_geotransform,
                      IntegerVector target_dim,
                      IntegerVector band) {
-  GDALDatasetH hSrcDS;
+
+  std::vector<GDALDatasetH> po_SrcDS(source_filename.size());
+
   GDALDatasetH hDstDS;
   GDALRasterBandH poBand, dstBand;
   GDALDriverH hDriver;
@@ -21,30 +34,37 @@ inline List gdal_warp_in_memory(CharacterVector source_filename,
 
   // Open input and output files.
   GDALAllRegister();
-  hSrcDS = GDALOpen( source_filename[0], GA_ReadOnly );
-  CPLAssert( hSrcDS != NULL );
+  Rcpp::CharacterVector oo;
+  std::vector <char *> oo_char = create_options(oo, true); // open options
+
+//  hSrcDS = GDALOpenEx( source_filename[0], GA_ReadOnly, NULL, oo_char.data(), NULL );
+
+  for (int i = 0; i < source_filename.size(); i++) {
+    po_SrcDS[i] = GDALOpenEx((const char *) source_filename[i], GA_ReadOnly, NULL, oo_char.data(), NULL);
+  }
+
+  CPLAssert( hSrcDS[0] != NULL );
 
   if (source_WKT[0].empty()) {
     // do nothing
   } else {
 
     Rprintf("setting projection");
-    GDALSetProjection( hSrcDS, source_WKT[0] );
+  //  GDALSetProjection( hSrcDS, source_WKT[0] );
   }
-  poBand = GDALGetRasterBand(hSrcDS, 1);
+  poBand = GDALGetRasterBand(po_SrcDS[0], 1);
 
   //TODO need type handling for nodata
   //int serr;
   //double no_data = GDALGetRasterNoDataValue(poBand, &serr);
   // Create output with same datatype as first input band.
   eDT = GDALGetRasterDataType(poBand);
-
   // Get output driver
   hDriver = GDALGetDriverByName( "MEM" );
 
   // Create the output file.
   hDstDS = GDALCreate( hDriver, "", target_dim[0], target_dim[1],
-                       GDALGetRasterCount(hSrcDS), eDT, NULL );
+                       GDALGetRasterCount(po_SrcDS[0]), eDT, NULL );
   CPLAssert( hDstDS != NULL );
 
   // Write out the projection definition.
@@ -53,62 +73,58 @@ inline List gdal_warp_in_memory(CharacterVector source_filename,
   for (int i = 0; i < 6; i++) GeoTransform[i] = target_geotransform[i];
   GDALSetGeoTransform( hDstDS, GeoTransform );
 
+  Rcpp::CharacterVector options;
+  std::vector <char *> options_char = create_options(options, true);
+  GDALWarpAppOptions* psOptions = GDALWarpAppOptionsNew(options_char.data(), NULL);
+
+  GDALDatasetH hOutDS = GDALWarp(NULL, hDstDS, 1, po_SrcDS.data(), psOptions, FALSE);
 
 
-  // Setup warp options.
-  GDALWarpOptions *psWarpOptions = GDALCreateWarpOptions();
-  psWarpOptions->hSrcDS = hSrcDS;
-  psWarpOptions->hDstDS = hDstDS;
-  psWarpOptions->nBandCount = 1;
-  psWarpOptions->panSrcBands =
-    (int *) CPLMalloc(sizeof(int) * psWarpOptions->nBandCount );
-  psWarpOptions->panSrcBands[0] = 1;
-  psWarpOptions->panDstBands =
-    (int *) CPLMalloc(sizeof(int) * psWarpOptions->nBandCount );
-  psWarpOptions->panDstBands[0] = 1;
-  psWarpOptions->pfnProgress = GDALTermProgress;
+  // // Establish reprojection transformer.
+  // psWarpOptions->pTransformerArg =
+  //   GDALCreateGenImgProjTransformer( hSrcDS,
+  //                                    GDALGetProjectionRef(hSrcDS),
+  //                                    hDstDS,
+  //                                    GDALGetProjectionRef(hDstDS),
+  //                                    FALSE, 0.0, 1 );
+  // psWarpOptions->pfnTransformer = GDALGenImgProjTransform;
+  //
+  // // Initialize and execute the warp operation.
+  // GDALWarpOperation oOperation;
+  // oOperation.Initialize( psWarpOptions );
+  // oOperation.ChunkAndWarpMulti( 0, 0,
+  //                               GDALGetRasterXSize( hDstDS ),
+  //                               GDALGetRasterYSize( hDstDS ) );
+  // GDALDestroyGenImgProjTransformer( psWarpOptions->pTransformerArg );
+  // GDALDestroyWarpOptions( psWarpOptions );
+  //
 
-  // Establish reprojection transformer.
-  psWarpOptions->pTransformerArg =
-    GDALCreateGenImgProjTransformer( hSrcDS,
-                                     GDALGetProjectionRef(hSrcDS),
-                                     hDstDS,
-                                     GDALGetProjectionRef(hDstDS),
-                                     FALSE, 0.0, 1 );
-  psWarpOptions->pfnTransformer = GDALGenImgProjTransform;
-
-  // Initialize and execute the warp operation.
-  GDALWarpOperation oOperation;
-  oOperation.Initialize( psWarpOptions );
-  oOperation.ChunkAndWarpImage( 0, 0,
-                                GDALGetRasterXSize( hDstDS ),
-                                GDALGetRasterYSize( hDstDS ) );
-  GDALDestroyGenImgProjTransformer( psWarpOptions->pTransformerArg );
-  GDALDestroyWarpOptions( psWarpOptions );
-  double *double_scanline;
-  double_scanline = (double *) CPLMalloc(sizeof(double)*
-    static_cast<unsigned long>(target_dim[0])*
-    static_cast<unsigned long>(target_dim[1]));
-  //GDALRasterIOExtraArg psExtraArg;
-  //INIT_RASTERIO_EXTRA_ARG(psExtraArg);
-
-  CPLErr err;
-  dstBand = GDALGetRasterBand(hDstDS, band[0]);
-
-  err = GDALRasterIO(dstBand,  GF_Read, 0, 0, target_dim[0], target_dim[1],
-                     double_scanline, target_dim[0], target_dim[1], GDT_Float64,
-                     0, 0);
+  // double *double_scanline;
+  // double_scanline = (double *) CPLMalloc(sizeof(double)*
+  //   static_cast<unsigned long>(target_dim[0])*
+  //   static_cast<unsigned long>(target_dim[1]));
+  // //GDALRasterIOExtraArg psExtraArg;
+  // //INIT_RASTERIO_EXTRA_ARG(psExtraArg);
+  //
+  //
+  // CPLErr err;
+  // dstBand = GDALGetRasterBand(hDstDS, band[0]);
+  //
+  // err = GDALRasterIO(dstBand,  GF_Read, 0, 0, target_dim[0], target_dim[1],
+  //                    double_scanline, target_dim[0], target_dim[1], GDT_Float64,
+  //                    0, 0);
   NumericVector res(target_dim[0] * target_dim[1]);
-  for (int i = 0; i < (target_dim[0] * target_dim[1]); i++) {
-    //  if (((float)double_scanline[i] == (float)no_data) || ISNAN(double_scanline[i])) {
-    //    res[i] = NA_REAL;
-    //  } else {
-    res[i] = double_scanline[i];
-    //  }
-  }
+  // for (int i = 0; i < (target_dim[0] * target_dim[1]); i++) {
+  //   //  if (((float)double_scanline[i] == (float)no_data) || ISNAN(double_scanline[i])) {
+  //   //    res[i] = NA_REAL;
+  //   //  } else {
+  //   res[i] = double_scanline[i];
+  //   //  }
+  // }
 
   GDALClose( hDstDS );
-  GDALClose( hSrcDS );
+  GDALClose( po_SrcDS[0] );
+
   Rcpp::List outlist(1);  //hardcode to 1 for now
   outlist[0] = res;
 
