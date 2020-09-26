9d14ad746df73b827a1a4f4dcfae8efaa5eb06d6
Michael Sumner <mdsumner@gmail.com>
2020-09-26 06:35:59 +0000

    example data

---
+++ data-raw/bluemarble.R
@@ -0,0 +1,7 @@
+## bluemarble Geotiff
+
+#https://neo.sci.gsfc.nasa.gov/view.php?datasetId=BlueMarbleNG-TB
+
+
+#gdal_translate data-raw/RenderData.tif inst/extdata/bluemarble.tif -co COMPRESS=JPEG
+
